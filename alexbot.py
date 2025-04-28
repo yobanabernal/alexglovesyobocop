23d8c8b26dec36d5861ea593fb7ed9590e739d1e
Yobana <79229122+yobanabernal@users.noreply.github.com>
2025-04-27 21:31:01 -0700

    Update alexbot.py

--- alexbot.py
+++ alexbot.py
@@ -81,6 +81,12 @@ win_counter = 0
 last_match_time = datetime.datetime.utcnow()
 last_goodnight_sent = None
 last_celebrated_match_id = None
+# --- Global Variables ---
+already_checked_matches = set()
+win_counter = 0
+last_match_time = datetime.datetime.utcnow()
+last_goodnight_sent = None
+last_celebrated_match_id = None
 ]
 
 # --- Global Variables ---
